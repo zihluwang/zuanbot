7cdbc4ac85c304e045ff7bfd3384f12979924bea
zihluwang <real@zihluwang.me>
2025-07-26 00:48:13 +0800

    feat: add resolving alias `@` to `./src`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react"
-import reactLogo from "./assets/react.svg"
 import { invoke } from "@tauri-apps/api/core"
-import "./App.css"
 
 enum Level {
   min = "min",
@@ -17,8 +15,8 @@ function App() {
   }
 
   function onChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    const value = event.target.value as Level;
-    setLevel(value);
+    const value = event.target.value as Level
+    setLevel(value)
   }
 
   return (
@@ -26,29 +24,15 @@ function App() {
       <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <select
-          value={level}
-          id="greet-input"
-          onChange={onChange}>
+        <select value={level} id="greet-input" onChange={onChange}>
           <option value={Level.min}>普通</option>
           <option value={Level.max}>火力全开</option>
         </select>
-        <button type="button" onClick={() => {
-          void getMessage()
-        }}>
+        <button
+          type="button"
+          onClick={() => {
+            void getMessage()
+          }}>
           Get Message
         </button>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -29,4 +35,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+}))
